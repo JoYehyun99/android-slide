9bee4271032f949a157fab36c6f343989d0254f1
jyh <jo990714@naver.com>
2023-07-19 14:50:04 +0900

    feat: 투명도 조절 구현

--- app/src/main/java/com/example/slideapp/SlideViewModel.kt
+++ app/src/main/java/com/example/slideapp/SlideViewModel.kt
@@ -7,18 +7,32 @@ import kotlin.random.Random
 
 class SlideViewModel : ViewModel() {
 
-    private val _squareColor: MutableLiveData<RGB> by lazy {
-        MutableLiveData<RGB>().apply {
-            value = getRandomColor()
+    private val _squareColor: MutableLiveData<ARGB> by lazy {
+        MutableLiveData<ARGB>().apply {
+            value = getRandomColor(10)
         }
     }
-    val squareColor: LiveData<RGB> = _squareColor
+    val squareColor: LiveData<ARGB> = _squareColor
 
-    private fun getRandomColor(): RGB {
-        return RGB(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256))
+    private fun getRandomColor(alpha: Int): ARGB {
+        return ARGB(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256), alpha)
     }
 
     fun changeBackgroundColor() {
-        _squareColor.value = getRandomColor()
+        _squareColor.value = getRandomColor(_squareColor.value!!.alpha)
+    }
+
+    fun addOpacity() {
+        if (_squareColor.value!!.alpha < 10) {
+            _squareColor.value =
+                _squareColor.value!!.copy(alpha = _squareColor.value!!.alpha.plus(1))
+        }
+    }
+
+    fun removeOpacity() {
+        if (_squareColor.value!!.alpha > 1) {
+            _squareColor.value =
+                _squareColor.value!!.copy(alpha = _squareColor.value!!.alpha.minus(1))
+        }
     }
 }
