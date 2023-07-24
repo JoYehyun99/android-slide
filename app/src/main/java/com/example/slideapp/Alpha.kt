f63bc63cf171f8b833a37e0d16f26cd4fff8f9e5
jyh <jo990714@naver.com>
2023-07-24 17:39:01 +0900

    feat: 사진 슬라이드 & 사진 변경 기능

--- app/src/main/java/com/example/slideapp/Alpha.kt
+++ app/src/main/java/com/example/slideapp/Alpha.kt
@@ -1,14 +1,14 @@
 package com.example.slideapp
 
-enum class Alpha(val code: String) {
-    ALPHA_1("1A"),
-    ALPHA_2("33"),
-    ALPHA_3("4D"),
-    ALPHA_4("66"),
-    ALPHA_5("80"),
-    ALPHA_6("99"),
-    ALPHA_7("B3"),
-    ALPHA_8("CC"),
-    ALPHA_9("E6"),
-    ALPHA_10("FF")
+enum class Alpha(val code: String, val value: Int) {
+    ALPHA_1("1A",25),
+    ALPHA_2("33",50),
+    ALPHA_3("4D",75),
+    ALPHA_4("66",100),
+    ALPHA_5("80",125),
+    ALPHA_6("99",150),
+    ALPHA_7("B3",175),
+    ALPHA_8("CC",200),
+    ALPHA_9("E6",225),
+    ALPHA_10("FF",255)
 }
